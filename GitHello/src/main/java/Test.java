498adb325dc63e47ae98627256dff5aa930ab9f1
Administrator <870110557@qq.com>
2020-11-01 10:43:27 +0800

    创建分支

--- GitHello/src/main/java/Test.java
+++ GitHello/src/main/java/Test.java
@@ -7,6 +7,8 @@ public class Test {
         System.out.println("第一次提交");
         System.out.println("第二次提交");
         System.out.println("第三次提交");
+        System.out.println("创建分支");
+
 
 
     }

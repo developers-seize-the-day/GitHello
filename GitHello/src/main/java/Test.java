28642d27f18232770ca8bd3e754c153c0d3dd69b
developers-seize-the-day <71753458+developers-seize-the-day@users.noreply.github.com>
2020-11-01 11:09:20 +0800

    Update Test.java

--- GitHello/src/main/java/Test.java
+++ GitHello/src/main/java/Test.java
@@ -10,8 +10,7 @@ public class Test {
         System.out.println("创建分支");
         System.out.println("主干添加剂");
         System.out.println("分支添加");
-
-
+         System.out.println("在线更新");
 
     }
 
